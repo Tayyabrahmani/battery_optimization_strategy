8b1fe88a35e280d4119ec25d80d98aa222b5fbbf
Tayyab Rahmani <tayyabrahmani@gmail.com>
2025-05-30 22:14:06 +0200

    feat: add LP model

--- main.py
+++ main.py
@@ -1,28 +1,68 @@
-from utils.utils import load_price_data, plot_results
+import argparse
+from utils.utils import (
+    calculate_profit,
+    save_results_to_csv,
+    load_price_data,
+    plot_results,
+)
 from models.simple_rule import RuleBasedSimulator
+from models.LP_optimization import LPBasedSimulator
+import os
+import pandas as pd
 
-def run_simulation(model_name, simulator_cls):
+capacity_mwh = 1.0
+power_mw = 0.5
+efficiency = 0.80
+degradation_cost_per_mwh = 0.01 / 1000
+grid_fee_per_mwh = 0.04 / 1000
+
+
+def run_simulation(model_name, simulator_cls, price_data):
     print(f"Running simulation: {model_name}")
     sim = simulator_cls(
-        capacity_mwh=1.0,
-        power_mw=0.5,
-        efficiency=0.90,
-        degradation_cost_per_kwh=0.01,
-        grid_fee_per_kwh=0.04
+        capacity_mwh=capacity_mwh,
+        power_mw=power_mw,
+        efficiency=efficiency,
+        degradation_cost_per_mwh=degradation_cost_per_mwh,
+        grid_fee_per_mwh=grid_fee_per_mwh,
     )
     sim.run_simulation(price_data)
     result_df = sim.to_dataframe()
-    plot_results(result_df)
-    print(f"{model_name} Total Profit: €", round(result_df['profit'].iloc[-1], 2))
+    result_df = calculate_profit(
+        df=result_df,
+        efficiency=efficiency,
+        degradation_cost_per_mwh=degradation_cost_per_mwh,
+        grid_fee_per_mwh=grid_fee_per_mwh,
+    )
+    save_results_to_csv(result_df, model_name)
+    return result_df
+
 
 if __name__ == "__main__":
-    price_data = load_price_data("data/Day-ahead_prices_202301010000_202501010000_Quarterhour.csv")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--simulate",
+        action="store_true",
+        help="Run a new simulation (default: use saved results)",
+    )
+    args = parser.parse_args()
 
-    # Register strategies
+    # Registered strategies
     models = {
-        "Rule-Based": RuleBasedSimulator
+        "Linear-Programming": LPBasedSimulator,
+        "Rule-Based": RuleBasedSimulator,
     }
 
     for name, cls in models.items():
-        run_simulation(name, cls)
+        result_path = f"results/{name.replace(' ', '_')}_results.csv"
+        if args.simulate or not os.path.exists(result_path):
+            price_data = load_price_data(
+                "data/Day-ahead_prices_202301010000_202501010000_Quarterhour.csv"
+            )
+            result_df = run_simulation(name, cls, price_data)
+        else:
+            result_df = pd.read_csv(result_path, parse_dates=["timestamp"])
+            print(f"Loaded cached results: {result_path}")
 
+        plot_results(result_df, title=f"{name} Operation")
+        print(f"{name} Total Profit: €{result_df['cumulative_profit'].iloc[-1]:,.2f}")
